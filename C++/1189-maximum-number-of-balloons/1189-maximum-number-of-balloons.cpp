3e6a492ae438d05bb4c4db158ee749fae74d90fc
its-not-worth-it <angshumoynandi19@gmail.com>
2026-07-13 03:33:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (87.94%) - LeetHub

--- C++/1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ C++/1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        string s="balloon";
-        int ans=INT_MAX;
-        vector<int> v(26,0), vis(26,0);
-        for(char i:text) vis[i-'a']++;
-        for( char i:s) v[i-'a']++;
-        for(int i=0;i<26;i++){
-            if(v[i]){
-                ans=min(ans,vis[i]/v[i]);
-            }
-        }
-        return ans;
+        vector<int> vis(26,0);
+        for(char i: text) vis[i-'a']++;
+        return min({
+            vis['b'-'a'], vis['a'-'a'], vis['l'-'a']/2, vis['n'-'a'], vis['o'-'a']/2
+        });
     }
 };
