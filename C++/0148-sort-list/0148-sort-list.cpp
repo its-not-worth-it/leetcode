de83ff74b9dc39115ca1e2e9a3188a09545f75ed
its-not-worth-it <angshumoynandi19@gmail.com>
2026-07-14 12:57:56 +0530

    Time: 87 ms (5.18%), Space: 86.5 MB (5.14%) - LeetHub

---
+++ C++/0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        return help(head);
+    }
+    ListNode* help(ListNode *head){
+        if(!head or !head->next) return head;
+
+        ListNode *f=head, *s=head, *temp=nullptr;
+        while(f and f->next){
+            f=f->next->next;
+            temp=s;
+            s=s->next;
+        }
+        temp->next=nullptr;
+
+        ListNode *l=help(head), *m=help(s);
+
+        return merge(l, m);
+    }
+    ListNode* merge(ListNode *l, ListNode *m){
+        ListNode *f=new ListNode(0); ListNode *s=f;
+        while(l and m){
+            if(l->val < m->val){
+                f->next=l;
+                l=l->next;
+            }else {
+                f->next=m;
+                m=m->next;
+            }
+            f=f->next;
+        }
+        while(l){
+            f->next=l;
+            l=l->next;
+            f=f->next;
+        }
+        while(m){
+            f->next=m;
+            m=m->next;
+            f=f->next;
+        }
+        // f->next=nullptr;
+        f=s->next;
+        delete s;
+        return f;
+    }
+};
