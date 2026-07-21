31b4cb0add2655298283bf5436a5caf8970e64ba
its-not-worth-it <angshumoynandi19@gmail.com>
2026-07-21 15:49:26 +0530

    Time: 8 ms (22.14%), Space: 19.1 MB (19.8%) - LeetHub

--- C++/0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ C++/0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -6,37 +6,25 @@ public:
         for(vector<int> i:p){
             graph[i[1]].emplace_back(i[0]);
         }
-        bool f=false;
         for(int i=0;i<n;i++){
-            if(!vis[i]) f=help(graph,vis,pvis,i,-1);
-            if(f) {
-                return {};
-            }
+            if(!vis[i] and help(graph,ans,vis,pvis,i,-1) ) return {};
+            
         }
-        for(int i=0;i<n;i++) vis[i]=false;
         
-        for(int i=0;i<n;i++){
-            if(!vis[i]) helper(graph,vis,ans,i);
-        }
 
         reverse(ans.begin(),ans.end());
         return ans;
     }
-    void helper(vector<vector<int>> &graph, vector<bool> &vis, vector<int> &ans, int i){
-        vis[i]=true;
-        for(int j:graph[i]){
-            if(!vis[j]) helper(graph,vis,ans,j);
-        }
-        ans.push_back(i);
-    }
-    bool help(vector<vector<int>> &graph, vector<bool> &vis, vector<bool> &pvis, int i, int p){
+    
+    bool help(vector<vector<int>> &graph,vector<int> &ans, vector<bool> &vis, vector<bool> &pvis, int i, int p){
         vis[i]=true; pvis[i]=true;
         for(int j:graph[i]){
             if(!vis[j]) {
-                if(help(graph,vis,pvis,j,i)) return true;
+                if(help(graph,ans,vis,pvis,j,i)) return true;
             }else if(vis[j] and pvis[j] ) return true;
         }
         pvis[i]=false;
+        ans.push_back(i);
         return false;
     }
 };
