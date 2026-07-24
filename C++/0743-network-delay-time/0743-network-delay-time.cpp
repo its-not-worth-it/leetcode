b1d24bf716d5b24c032b535d004c1c2c31f4eabf
its-not-worth-it <angshumoynandi19@gmail.com>
2026-07-25 03:43:13 +0530

    Time: 102 ms (9.91%), Space: 48.2 MB (7.69%) - LeetHub

--- C++/0743-network-delay-time/0743-network-delay-time.cpp
+++ C++/0743-network-delay-time/0743-network-delay-time.cpp
@@ -5,16 +5,19 @@ public:
         for(vector<int> i:times){
             g[i[0]].push_back({i[1],i[2]});
         }
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
-        pq.push({0,k});
+        set<pair<int,int>> pq;
+        pq.insert({0,k});
         vector<int> dist(n+1,1e9); dist[0]=-1; dist[k]=0;
         while(!pq.empty()){
-            int td=pq.top().first, node=pq.top().second;
-            pq.pop();
+            int td=pq.begin()->first, node=pq.begin()->second;
+            pq.erase(pq.begin());
             for(auto it: g[node]){
                 if(td+it.second < dist[it.first]){
+                    if(dist[it.first] != 1e9){
+                        pq.erase({dist[it.first],it.first});
+                    }
                     dist[it.first]=td+it.second;
-                    pq.push({dist[it.first],it.first});
+                    pq.insert({dist[it.first],it.first});
                 }
             }
         }
