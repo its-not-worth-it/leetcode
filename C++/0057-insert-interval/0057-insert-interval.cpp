c7ca49499226f76ad33e1ee6be453d9d95a36405
its-not-worth-it <angshumoynandi19@gmail.com>
2026-08-01 01:26:00 +0530

    Time: 0 ms (100%), Space: 21.7 MB (76.76%) - LeetHub

---
+++ C++/0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& nit) {
+        vector<vector<int>> ans;
+        int i=0, n=intervals.size();
+        for(;i<n;i++){
+            if(intervals[i][0] > nit[0]) break;
+        }
+        // cout<<i;
+
+        for(int j=0;j<i;j++) ans.push_back(intervals[j]);
+
+        if(ans.empty() or ( ans.back()[1]< nit[0]) ) ans.push_back(nit);
+        else ans.back()[1]=max(ans.back()[1], nit[1]);
+
+        while(i<n){
+            if(ans.back()[1] >= intervals[i][0]) ans.back()[1]=max(ans.back()[1], intervals[i][1]);
+            else ans.push_back(intervals[i]);
+            i++;
+        }
+
+
+
+        return ans;
+    }
+};
